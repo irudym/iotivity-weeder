8076e410e96f93ead4c9605e8b8f8fb2fa9b7017
irudym <irudym@gmail.com>
2016-04-23 22:21:22 +0300

    Changed main function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <signal.h>
+
+#include "weederServer.h"
 
 using namespace std;
 
+int quit = 0;
+
+void handle_signal(int signal)
+{
+    quit = 1;
+}
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Weeder IoT Server v0.1" << endl;
+
+    struct sigaction sa;
+    sigfillset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = handle_signal;
+    sigaction(SIGINT, &sa, NULL);
+    cout << "Press Ctrl-C to quit...." << endl;
+    WeederIoTServer server;
+    do
+    {
+        usleep(2000000);
+    }
+    while (quit != 1);
     return 0;
+
 }
